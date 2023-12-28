0dfdb5c660ed97c49e0f2cf07cb3d79ce0827a46
ganeshvalere <147713881+ganeshvalere@users.noreply.github.com>
2023-12-28 11:34:45 +0000

    03_object.js

--- 02_basic/03_object.js
+++ 02_basic/03_object.js
@@ -2,4 +2,41 @@
 
 //object
 
-const JsUser
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Ganesh",
+    "full name": "Ganesh Vadiya",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "ganesh@google.com",
+    isLoggedIn: false,
+    lastLoginDats: ["Monday", "Saterday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "ganesh@chatghpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "ganesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello Js User");
+}
+
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js User, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
